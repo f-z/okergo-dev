30331ef5fa1b17348559997aa1cb06b18e922cca
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-10-16 23:05:51 +0100

    Added engineer tee rank, student rank roles, and permission to start private threads for everyone

--- misago/users/migrations/0004_default_ranks.py
+++ misago/users/migrations/0004_default_ranks.py
@@ -3,7 +3,8 @@ from django.utils.translation import gettext
 
 from ...core.utils import slugify
 
-_ = lambda s: s
+
+def _(s): return s
 
 
 def create_default_ranks(apps, schema_editor):
@@ -26,11 +27,19 @@ def create_default_ranks(apps, schema_editor):
         order=1,
     )
 
+    engineers_tee = Rank.objects.create(
+        name="Μηχανικοί Τ.Ε.",
+        slug=slugify("Μηχανικοί Τ.Ε."),
+        title="Μηχανικός Τ.Ε.",
+        is_tab=True,
+        order=2,
+    )
+
     students = Rank.objects.create(
         name="Φοιτητές",
         slug=slugify("Φοιτητές"),
         title="Φοιτητής",
-        order=2,
+        order=3,
     )
 
     members = Rank.objects.create(
@@ -38,20 +47,31 @@ def create_default_ranks(apps, schema_editor):
         slug=slugify("Ιδιώτες"),
         title="Πελάτης",
         is_default=True,
-        order=3,
+        order=4,
     )
 
     Role = apps.get_model("misago_acl", "Role")
 
     team.roles.add(Role.objects.get(name="Διαχειριστής αγγελιών"))
     team.roles.add(Role.objects.get(name="Μπορεί να αποκλείει χρήστες"))
-    team.roles.add(Role.objects.get(name="Διαχειριστής συνομιλιών για ανάθεση εργασίας"))
+    team.roles.add(Role.objects.get(
+        name="Διαχειριστής συνομιλιών για ανάθεση εργασίας"))
 
     engineers.roles.add(Role.objects.get(name="Μηχανικός"))
-    engineers.roles.add(Role.objects.get(name="Συμμετοχή σε συνομιλία για ανάθεση εργασίας"))
-   
+    engineers.roles.add(Role.objects.get(
+        name="Ξεκίνημα συνομιλίας για ανάθεση εργασίας"))
+
+    engineers_tee.roles.add(Role.objects.get(name="Μηχανικός"))
+    engineers_tee.roles.add(Role.objects.get(
+        name="Ξεκίνημα συνομιλίας για ανάθεση εργασίας"))
+
+    students.roles.add(Role.objects.get(name="Μηχανικός"))
+    students.roles.add(Role.objects.get(
+        name="Ξεκίνημα συνομιλίας για ανάθεση εργασίας"))
+
     members.roles.add(Role.objects.get(name="Πελάτης"))
-    members.roles.add(Role.objects.get(name="Ξεκίνημα συνομιλίας για ανάθεση εργασίας"))
+    members.roles.add(Role.objects.get(
+        name="Ξεκίνημα συνομιλίας για ανάθεση εργασίας"))
 
 
 class Migration(migrations.Migration):
